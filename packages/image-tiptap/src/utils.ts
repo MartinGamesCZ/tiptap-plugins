afb4577e91885ce1980434b975704dcae389d829
Harsh <harshpareek91@gmail.com>
2024-09-26 06:15:10 +0530

    fix: package rename and image margin top issue

--- packages/image-tiptap/src/utils.ts
+++ packages/image-tiptap/src/utils.ts
@@ -2,7 +2,7 @@ import type { Alignment } from "./types";
 
 // This will be used to calculate the alignment of the data in the table
 export const alignmentVariants: Record<Alignment, string> = {
-  center: `margin: 0 auto`,
+  center: `margin: auto auto`,
   left: `margin-right: auto`,
   right: `margin-left: auto`,
 };
